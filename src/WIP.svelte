a7bad4543945cf64b02101c096e46877c9a056cd
NOOBDY <htommyf12@gmail.com>
2021-01-16 21:12:34 +0800

    stripped WIP screen into a seperate file

---
+++ src/WIP.svelte
@@ -0,0 +1,27 @@
+<div id="description">
+  <h1>Hi! This is my GitHub Page</h1>
+  <h2>This site is still under development</h2>
+  <p>
+    View my profile and other projects here <a href="https://github.com/NOOBDY"
+      >here</a
+    >
+  </p>
+</div>
+<div id="bg-text">Work In Progress...</div>
+
+<style>
+  #description {
+    text-align: center;
+  }
+
+  #bg-text {
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    font-size: 60px;
+    transform: translate(-50%, -50%);
+    font-family: "PT Mono", monospace;
+    color: #b6b6b6;
+  }
+</style>
