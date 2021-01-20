05bc48acac6e319c4a8ba3ad58f5480a75b9326d
NOOBDY <htommyf12@gmail.com>
2021-01-20 16:40:17 +0800

    added basic about page styling

--- src/About.svelte
+++ src/About.svelte
@@ -1,3 +1,16 @@
 <div class={$$props.class} id="about">
-  <h1>blah blah blah</h1>
+  <h1 class="header">about</h1>
 </div>
+
+<style>
+  #about {
+    background-color: #000000;
+  }
+  #about h1 {
+    color: #ffffff;
+    position: absolute;
+    top: 20%;
+    left: 20%;
+    transform: translate(-50%, -50%);
+  }
+</style>
