fc4cd3fc2fc940034c89c07775603a29c22cb3f9
NOOBDY <htommyf12@gmail.com>
2021-04-19 09:46:05 +0800

    changed wip screen to be responsive

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,8 @@
     <h1>Hi! This is my GitHub Page</h1>
     <h2>This site is still under development</h2>
     <p>
-      View my profile and other projects here <a
-        href="https://github.com/NOOBDY">here</a
-      >
+      View my profile and other projects
+      <a href="https://github.com/NOOBDY">here</a>
     </p>
   </div>
   <div id="bg-text">Work In Progress...</div>
@@ -14,16 +13,39 @@
 <style>
   #description {
     text-align: center;
+    margin-top: 4rem;
+  }
+
+  #description h1 {
+    font-size: 5vmin;
+  }
+
+  #description h2 {
+    font-size: 4vmin;
+  }
+
+  #description p {
+    font-size: 3vmin;
   }
 
   #bg-text {
+    /* center text */
     text-align: center;
     position: absolute;
     top: 50%;
     left: 50%;
-    font-size: 60px;
     transform: translate(-50%, -50%);
+
+    /* font */
+    font-size: 5vw;
     font-family: "PT Mono", monospace;
+    white-space: nowrap;
     color: #b6b6b6;
+
+    /* unselectable */
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
   }
 </style>
