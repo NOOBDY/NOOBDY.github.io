3715322cafb4be1e802fca6bda987d96d25de670
NOOBDY <htommyf12@gmail.com>
2021-04-20 09:36:22 +0800

    changed home screen to be responsive

--- src/Home.svelte
+++ src/Home.svelte
@@ -6,15 +6,25 @@
 </div>
 
 <style>
-  #introduction {
+  /*#introduction {
     position: absolute;
     top: 50%;
     left: 30%;
-    font-size: 50px;
+    font-size: 3.5vw;
     transform: translate(-50%, -50%);
     color: #000000;
+  }*/
+  #home {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+  }
+  #introduction {
+    font-size: 3.5vw;
+    margin-left: 15vmin;
   }
   #name {
     font-size: 80px;
+    font-size: 6vw;
   }
 </style>
