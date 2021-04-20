579c21be3516948c739947500c57776fb5086428
Tommy Huang <htommyf12@gmail.com>
2021-04-20 16:41:50 +0800

    deleted redundant code

--- src/About.svelte
+++ src/About.svelte
@@ -1,25 +1,22 @@
 <div class={$$props.class} id="about">
   <div>
-    <h1 class="title">about me</h1>
+    <h1 id="title">about me</h1>
     <p class="subtitle">
       i like to watch movies,<br />
       solve problems,<br />
       and build cool stuff.
     </p>
-    <p class="content">
-      i have been writing code since 15, lorem ipsum dolor sit amet, consectetur
-      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua. ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat. duis aute irure dolor in
-      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-      pariatur. excepteur sint occaecat cupidatat non proident, sunt in culpa
-      qui officia deserunt mollit anim id est laborum.
-    </p>
+    <p class="content" />
   </div>
 </div>
 
 <style>
-  .title {
+  #about {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #about #title {
     font-size: 150px;
     position: absolute;
     top: 20%;
