ae7bbdda5d5e9d52cf2aecd2fe4d00826374f1c9
NOOBDY <htommyf12@gmail.com>
2021-04-24 13:24:14 +0800

    created basic working project template

---
+++ src/ProjectTemplate.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Projects from "./Projects.svelte";
+</script>
+
+<div class="project">
+    <a href={$$props.github_src}>
+        <img src={$$props.img_src} alt="" />
+    </a>
+    <h1>{$$props.title}</h1>
+    <p>{$$props.p}</p>
+</div>
+
+<style>
+    .project {
+        text-align: center;
+    }
+    img {
+        height: 10rem;
+        width: 10rem;
+        object-fit: contain;
+    }
+</style>
